0afe31a180392c5149ffb05ebc16af9077580767
sajjadhaider <sajjadkazmi962@gmail.com>
2024-10-02 22:33:06 +0500

    first commit

---
+++ src/main/java/com/_4/auth/AuthApplication.java
@@ -0,0 +1,13 @@
+package com._4.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
